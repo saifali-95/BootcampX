cf260681ab84e721b3562d01f73a1361de2f16f7
Saif Ali <saifali7646@gmail.com>
2021-08-18 00:02:15 +0000

    total teacher's assistance_request

---
+++ 4_queries/total_teacher_assistance_requests.sql
@@ -0,0 +1,7 @@
+SELECT
+  COUNT(assistance_requests.*) as total_assistances,
+  teachers.name as name
+FROM assistance_requests JOIN teachers ON teacher_id = teachers.id
+WHERE name = 'Waylon Boehm'
+GROUP BY name;
+
