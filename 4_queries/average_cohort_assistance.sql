7e70afb914aa9887f86ffbfd328a3b0e568baa4c
Saif Ali <saifali7646@gmail.com>
2021-08-18 01:53:19 +0000

    average cohort assistance

---
+++ 4_queries/average_cohort_assistance.sql
@@ -0,0 +1,10 @@
+SELECT 
+  cohorts.name, 
+  AVG(assistance_requests.completed_at - assistance_requests.started_at) as average_assistance_time
+
+FROM assistance_requests 
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time;
+
